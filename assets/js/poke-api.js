53134ce356e56882748aea93b428f7cbc66c3d20
andersonprovox <andersongfonseca@gmail.com>
2023-11-29 09:49:52 -0300

    personalizando os dados para manipulação na aplicação

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,10 +3,35 @@
 
 const PokeApi = {}
 
-PokeApi.getPokemons = function (offset = 0 , limit = 10) {
+function convertPokeApiDetailToPokemon(pokemonDetail) {
+  const pokemon = new Pokemon()
+  pokemon.number = pokemonDetail.order
+  pokemon.name = pokemonDetail.name
+
+  const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+  const [type] = types
+
+  pokemon.types = types
+  pokemon.type = type
+
+  pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+
+  return pokemon
+}
+
+PokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+}
+
+PokeApi.getPokemons = function (offset = 0 , limit = 5) {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
   return fetch(url)
     .then( (response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+    .then((pokemons) => pokemons.map(PokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
 }
